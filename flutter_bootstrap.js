7de6ad54c25a221c8628395c5a913131de6b2da9
WesselvanDam <WesselvanDam@users.noreply.github.com>
2024-09-18 17:57:23 +0000

    deploy: 1df943e2207ca7be81a69e8eb102fd2eb68f8e68

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "297710540"
+    serviceWorkerVersion: "4009426527"
   }
 });
